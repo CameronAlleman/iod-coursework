3bf3294a0f72c50af2cd640efd27637d534d9af5
CameronAlleman <callem6@gmail.com>
2024-12-14 16:21:23 -0600

    add module 1

---
+++ module1/LabExercises/excersise5.js
@@ -0,0 +1,7 @@
+let animals = ['dog' , 'cat' , 'chicken' , 'cow', 'pig'];
+animals[1] = 'horse';
+animals[4] = 'duck';
+animals.unshift('turkey');
+let firstanimals = animals.shift()
+console.log('firstanimals')
+console.log('animals');
---
+++ module1/LabExercises/exersise4.js
@@ -0,0 +1,41 @@
+function add(a, b) {
+   let result = a + b;
+   console.log('result of a + b');
+   return result;
+}
+
+
+function subtract(a, b) {
+    let result2 = a - b;
+    console.log('result of a - b')
+    return result2;
+}
+
+
+function multiply(a, b) {
+    let result3 = a * b;
+    console.log('result of a *b')
+    return result3;
+}
+
+
+function divide(a, b) {
+    let result4 = a / b;
+    console.log('result of a / b')
+    return result4;
+}
+
+
+
+
+
+
+
+
+
+
+function hello(name) {
+    let message = "Hello" + name;
+    console.log(message);
+}
+hello("cameron");
