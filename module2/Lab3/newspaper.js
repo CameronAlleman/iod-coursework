af33b3e37cf647056b9150d6bbf1fedc8dd8f9f2
CameronAlleman <callem6@gmail.com>
2025-01-25 14:00:05 -0600

    update module2

--- module2/Lab3/newspaper.js
+++ module2/Lab3/newspaper.js
@@ -1,11 +1,11 @@
 function toggleDropdown() { 
     var dropdown = document.getElementById("dropdown");
     
-    if (dropdown.style.display === "flex") 
+    if (dropdown.style.display === "none") 
     
-        { dropdown.style.display = "none"; 
+        { dropdown.style.display = "flex"; 
 
-    } else { dropdown.style.display = "flex"; 
+    } else { dropdown.style.display = "none"; 
 
     } 
 }
