ee7a40595106c08a18661f3572b36141f2b7a794
CameronAlleman <callem6@gmail.com>
2025-01-25 19:14:02 -0600

    Delete module1 directory

--- module1/LabExercises/excersise5.js
+++
@@ -1,7 +0,0 @@
-let animals = ['dog' , 'cat' , 'chicken' , 'cow', 'pig'];
-animals[1] = 'horse';
-animals[4] = 'duck';
-animals.unshift('turkey');
-let firstanimals = animals.shift()
-console.log('firstanimals')
-console.log('animals');
