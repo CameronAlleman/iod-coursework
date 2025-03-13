ba1bf8ed133ae26e645ab47356a0838d89af9f02
CameronAlleman <callem6@gmail.com>
2025-03-13 18:49:45 -0500

    add module5

---
+++ module5/myapp/libraries/Calculator.js
@@ -0,0 +1,31 @@
+class Calculator {
+    constructor() {
+        const { v4: uuidv4 } = require('uuid');
+        this.id = uuidv4();
+    }
+    #log = (value) => {
+    console.log(`[Calculator :${this.id}]:${value}`)
+    }
+
+    add(num1, num2) {
+        const value = num1 + num2
+        this.#log(value)
+        return value;
+    }
+    subtract(num1, num2) {
+        const value = num1 - num2
+        this.#log(value)
+        return value;
+    }
+    multiply(num1, num2) {
+        const value = num1 * num2
+        this.#log(value)
+        return value;
+    }
+    divide(num1, num2) {
+        const value = num1 / num2
+        this.#log(value)
+        return value;
+    }
+ }
+ module.exports = Calculator
